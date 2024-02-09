c0ed0a86577f6cf929839f1a2ce30f0bbab2a2d7
vitterre <bagatovbb@gmail.com>
2024-02-09 14:36:34 +0300

    build: add lab1 module

---
+++ lab1/src/main/java/com/technokratos/agona/Main.java
@@ -0,0 +1,10 @@
+package com.technokratos.agona;
+
+import lombok.val;
+
+public class Main {
+    public static void main(String[] args) {
+        val message = "hello";
+        System.out.println(message);
+    }
+}
